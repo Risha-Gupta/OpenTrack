cdc49c90f8eb660e357a4c01b99a6e8587596560
Risha <rishagupta87503@gmail.com>
2026-04-27 10:08:34 +0530

    Add Swagger/OpenAPI docs config with JWT bearer auth

---
+++ src/main/java/com/opentrack/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.opentrack.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openTrackOpenAPI() {
+        final String securitySchemeName = "BearerAuth";
+        return new OpenAPI()
+            .info(new Info()
+                .title("OpenTrack API")
+                .description("Open Source Contribution Tracker REST API")
+                .version("v1.0.0")
+                .contact(new Contact().name("OpenTrack").url("https://github.com/yourusername/opentrack"))
+                .license(new License().name("MIT")))
+            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+            .components(new Components()
+                .addSecuritySchemes(securitySchemeName,
+                    new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+    }
+}
